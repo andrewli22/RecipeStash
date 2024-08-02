5f488b38bcb7ab3ed8144d20e5c9ba0a1d73e454
Andrew Li <andrew.li2277@gmail.com>
2024-08-02 20:59:31 +1000

    .

--- frontend/src/pages/RecipePage.jsx
+++ frontend/src/pages/RecipePage.jsx
@@ -11,38 +11,38 @@ export const RecipePage = () => {
   const [directions, setDirections] = useState();
   const [numServing, setNumServing] = useState();
 
-  const fetchRecipeInfo = useCallback(async () => {
+  const fetchRecipeInfo = async () => {
     try {
       const response = await fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${KEY}`);
       const recipeInfo = await response.json();
-      const getRecipeInfo = {
-        credits: recipeInfo.creditsText,
-        healthScore: recipeInfo.healthScore,
-        image: recipeInfo.image,
-        time: recipeInfo.readyInMinutes,
-        servingSize: recipeInfo.servings
-      };
+      const { creditsText, healthScore, image, readyInMinutes, servings, extendedIngredients, instructions } = recipeInfo;
+      setInfo({
+        credits: creditsText,
+        healthScore: healthScore,
+        image: image,
+        time: readyInMinutes,
+        servingSize: servings
+      });
 
-      const getIngredients = recipeInfo.extendedIngredients.map((item) => ({
-        original: item.original.replace(/^[–-]\s*/, '').trim(),
-        image: item.image
-      }));
-      const sanitiseInstructions = DOMPurify.sanitize(recipeInfo.instructions);
+      setIngredients(extendedIngredients.map(({ original, image }) => ({
+        original: original.replace(/^[–-]\s*/, '').trim(),
+        image: image
+      })));
+      
+      const sanitiseInstructions = DOMPurify.sanitize(instructions);
       const instructionArr = sanitiseInstructions
-        .replaceAll(/(<([^>]+)>)/ig, '')
+        .replaceAll(/<[^>]+>/g, '')
         .split('.')
         .filter(instruction => instruction.trim() !== '' && !/^\d+$/.test(instruction.trim()))
         .map(instruction => instruction.trim());
-      instructionArr.pop();
-      setDirections(instructionArr);
-      setInfo(getRecipeInfo);
-      setIngredients(getIngredients);
+      
+      setDirections(instructionArr.slice(0, -1));
       setDietary(recipeInfo.diets);
       setNumServing(recipeInfo.servings);
     } catch (error) {
       console.log(error);
     }
-  }, [recipeId]);
+  };
 
   useEffect(() => {
     fetchRecipeInfo();
@@ -79,7 +79,12 @@ export const RecipePage = () => {
                   {info.time} {info.time < 2 ? 'min' : 'mins'}
                 </div>
               </div>
-              <div className='flex flex-col'>
+            </div>
+          </div>
+          <div className='ml-5'>
+            <div className='flex text-xl justify-center'>Ingredients</div>
+            <div className='flex flex-col justify-center'>
+              <div className='flex flex-col w-fit'>
                 <div>
                   Serving Size
                 </div>
@@ -105,20 +110,17 @@ export const RecipePage = () => {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className='ml-5'>
-            <div className='flex text-xl justify-center'>Ingredients</div>
-            <div className='ml-5 w-1/2'>
-              <ul className='grid grid-cols-2'>
-                {ingredients.map((item, id) => {
-                  return (
-                    <li className='text-left mb-2' key={id}>
-                      {item.original}
-                    </li>
-                  );
-                })}
-              </ul>
+              <div className='w-2/3'>
+                <ul className='grid grid-cols-2 list-disc'>
+                  {ingredients.map((item, id) => {
+                    return (
+                      <li className='text-left mb-2' key={id}>
+                        {item.original}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
             <div className=' flex text-xl'>Instructions</div>
             <div className='ml-5 w-full'>
