4f2a27f5e3cc2322e894585ddb566949020a073a
Andrew Li <andrew.li2277@gmail.com>
2025-09-14 11:40:50 +1000

    Use Vercel React analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import { Recipes } from './pages/Recipes';
 import { Search } from './pages/Search';
 import { Ingredients } from './pages/Ingredients';
 import { RecipePage } from './pages/RecipePage';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Analytics />
         <Route path='/' element={<Main />}/>
         <Route path='/recipe' element={<Recipes />}/>
         <Route path='/search' element={<Search />}/>
         <Route path='/ingredients' element={<Ingredients />}/>
         <Route path='/recipe/:recipeId/:title' element={<RecipePage />}/>
       </Routes>
+      <Analytics />
     </BrowserRouter>
   )
 }
